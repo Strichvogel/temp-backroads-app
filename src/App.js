2724681496b1f893fb2b296327100cdcc733f64e
Zhexi Deng <Jessiedeng1994@gmail.com>
2025-01-06 22:43:59 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+
+      {/* <script src="./js/app.js"></script> */}
+    </>
+  );
+}
+
+export default App;
